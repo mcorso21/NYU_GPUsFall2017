8ed1807bd010bede2202444b3196ffabd3c1e107
mcorso21 <mcorso21@gmail.com>
2017-11-07 18:40:12 -0500

    Value

--- src/occupancy.cu
+++ src/occupancy.cu
@@ -25,18 +25,38 @@ void getGPU(int *);
 
 int main() {
 
+	// GOAL: CURRENT WARPS / MAX WARPS PER SM
+	/*
+		IDEA #1
+
+		Create a block of 2048, which is 100% warp occupancy (2048/32 [warp size] = 64 [max # warps])
+
+		In bash script
+			blockSize = 2048
+			while (blockSize >= 32)
+				Call occupancy.cu with block/grid size
+			
+		In occupancy.cu
+			gridSize = arg
+			blockSize = arg
+
+			call kernel function with grid/block size 
+
+	*/
+
 	setlocale(LC_NUMERIC, "");
 
-    // DEVICE, MAX BLOCK SIZE, #SMS, MAX GRID SIZE, MAX THREADS, WARP SIZE, # REGISTERS PER BLOCK
-    int devInfo [7];
+    // DEVICE, MAX BLOCK SIZE, #SMS, MAX GRID SIZE, MAX THREADS, WARP SIZE, # REGISTERS PER BLOCK, MAJOR, MINOR
+    int devInfo [9];
     getGPU(devInfo);
 
     // IF MULTIPLE DEVICES PRESENT, USE DEVICE WITH LARGEST BLOCK SIZE
     cudaSetDevice(devInfo[0]);
 
     if (testing) {
-        printf("\nGPU Info:\n\t%-15s %'d\n\t%-15s %'d\n\t%-15s %'d\n\t%-15s %'d\n\t%-15s %'d\n\t%-15s %'d\n\t%-15s %'d\n", 
+        printf("\nGPU Info:\n\t%-15s %'d\n\t%-15s %d.%d\n\t%-15s %'d\n\t%-15s %'d\n\t%-15s %'d\n\t%-15s %'d\n\t%-15s %'d\n\t%-15s %'d\n", 
             "Device ID", devInfo[0],
+            "Compute C.", devInfo[7], devInfo[8], 
             "Grid Size", devInfo[3],
             "Block Size", devInfo[1],
             "Max Threads", devInfo[4],
@@ -121,4 +141,6 @@ void getGPU(int * devInfo) {
     devInfo[4] = maxThreads; 	// Max # threads
     devInfo[5] = warpSize; 		// Warp size
     devInfo[6] = regsPerBlock;	// # Registers per block
+    devInfo[7] = dev_prop.major;
+    devInfo[8] = dev_prop.minor;
 }
